f108f9f32f2563bfc59f101e5e74dcadad8eb1f9
Shreya Jaiswal <shreyaa.we@gmail.com>
2026-08-12 20:13:38 +0530

    few changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wails from "@wailsio/runtime/plugins/vite";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "127.0.0.1",
     port: Number(process.env.WAILS_VITE_PORT) || 9245,
     strictPort: true,
   },
-  plugins: [react(), wails("./bindings"),tailwindcss()],
+
+  plugins: [
+    react(),
+    wails("./bindings"),
+    tailwindcss(),
+  ],
 });
